6031e54bc29cd772eec35fd051aeb297c223cd47
Tony Liu <tliu.task@gmail.com>
2013-06-19 00:29:02 -0700

    add new projects CSharpDev

---
+++ CSharpDev/CSharpDev.Library/Chapter1/OverloadOperator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpDev.Library.Chapter1
+{
+	public class OverloadOperator
+	{
+		private class Word
+		{
+			public string Text { get; set; }
+
+			public static string operator +(Word w1, Word w2)
+			{
+				return w1.Text + " " + w2.Text;
+			}
+
+			public static string operator +(Word w1, int w2)
+			{
+				return w1.Text + " " + w2.ToString();
+			}
+
+			public override string ToString()
+			{
+				return this.Text;
+			}
+		}
+
+		public static void Run()
+		{
+			Word w1 = new Word() { Text = "Tony" };
+			Word w2 = new Word() { Text = "Luke" };
+
+			Console.WriteLine("Word + Word: {0}", w1 + w2);
+			Console.WriteLine("Word + Int: {0}", w2 + 1);
+		}
+	}
+}
---
+++ CSharpDev/CSharpDev/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSharpDev.Library.Chapter1;
+
+namespace CSharpDev
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+            Console.WriteLine("Hello GitHub!");
+			Console.WriteLine("Enter an option to run:");
+			Console.WriteLine("1. Operator Overload.");
+			Console.WriteLine();
+			Console.Write("====>	");
+			string key = Console.ReadLine();
+
+			Console.WriteLine();
+			Console.WriteLine("============================== Results =============================");
+			switch(key)
+			{
+				case "1":
+					OverloadOperator.Run();
+					break;
+				default:
+					Console.WriteLine("Invalid option.");
+					break;
+			}
+			Console.WriteLine("========================= End of Results ===========================");
+			Console.WriteLine();
+		}
+	}
+}
