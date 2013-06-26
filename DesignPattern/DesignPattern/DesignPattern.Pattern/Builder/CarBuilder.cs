89567d06e2d09dd769e42da1770818178575d0e5
Tony Liu <tliu.task@gmail.com>
2013-06-26 00:27:52 -0700

    add carbuilder (uncompleted)

---
+++ DesignPattern/DesignPattern/DesignPattern.Pattern/Builder/CarBuilder.cs
@@ -0,0 +1,49 @@
+﻿using DesignPattern.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.Pattern.Builder
+{
+    public class CarBuilder : VehicleBuilder
+    {
+        private AbstractCar carInProgress;
+
+        public CarBuilder(AbstractCar car)
+        {
+            this.carInProgress = car;
+        }
+
+        public override void BuildBody()
+        {
+            base.BuildBody();
+        }
+
+        public override void BuildBoot()
+        {
+            base.BuildBoot();
+        }
+
+        public override void BuildChassis()
+        {
+            base.BuildChassis();
+        }
+
+        public override void BuildPassengerArea()
+        {
+            base.BuildPassengerArea();
+        }
+
+        public override void BuildWindows()
+        {
+            base.BuildWindows();
+        }
+
+        public override IVehicle Vehicle
+        {
+            get { return this.carInProgress; }
+        }
+    }
+}
